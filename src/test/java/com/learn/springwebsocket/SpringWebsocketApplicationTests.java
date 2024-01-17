aead25854a858cfe4100ea3f1a8aea80d37bfd8b
Vaibhav Agarwal <agarwalvaibhav041@gmail.com>
2024-01-17 17:09:25 +0530

    added the web socket

---
+++ src/test/java/com/learn/springwebsocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
